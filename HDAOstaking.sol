77353fb98dbb9517a1a7a84816a430f19a4ca275
Barbarian Finance <95499220+barbarian-fi@users.noreply.github.com>
2022-01-03 19:42:11 -0500

    Update HDAOstaking.sol

--- HDAOstaking.sol
+++ HDAOstaking.sol
@@ -11,6 +11,10 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
+
+
 
 
 
