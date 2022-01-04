04f5f4e7d5acdbec83fe17b4bd4967114ade4f3c
Barbarian Finance <95499220+barbarian-fi@users.noreply.github.com>
2022-01-03 19:33:24 -0500

    Update HDAOstaking.sol

--- HDAOstaking.sol
+++ HDAOstaking.sol
@@ -3,3 +3,5 @@ pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
